69b23133f1effd6f2d6f48b115b1be81c98591ee
George Salah <georgesalah290@gmail.com>
2026-08-11 20:09:00 +0300

    Point akkad-v2.js to fixed akkad.js (goTop gutted)

--- src/modules/akkad-v2.js
+++ src/modules/akkad-v2.js
@@ -15,7 +15,7 @@
    ========================================= */
 (function () {
     var s = document.createElement("script");
-    s.src = "https://cdn.jsdelivr.net/gh/gorgeousDev/jsDeilver@7c6789853e562d9e7fdde30cb5807cae9e9010cc/src/modules/akkad.js";
+    s.src = "https://cdn.jsdelivr.net/gh/gorgeousDev/jsDeilver@7ab8a9a/src/modules/akkad.js";
     s.defer = true;
     document.head.appendChild(s);
 })();
