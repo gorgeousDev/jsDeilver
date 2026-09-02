560bae2b7f3e9f38cf96a7292e314e381f293cdb
George Salah <georgesalah290@gmail.com>
2026-09-03 00:18:05 +0300

    Update akkad.js pinned hash to include scroll-to-top fix

--- src/modules/akkad-v2.js
+++ src/modules/akkad-v2.js
@@ -15,7 +15,7 @@
    ========================================= */
 (function () {
     var s = document.createElement("script");
-    s.src = "https://cdn.jsdelivr.net/gh/gorgeousDev/jsDeilver@f0b7bfc/src/modules/akkad.js";
+    s.src = "https://cdn.jsdelivr.net/gh/gorgeousDev/jsDeilver@ecc28b9/src/modules/akkad.js";
     s.defer = true;
     document.head.appendChild(s);
 })();
